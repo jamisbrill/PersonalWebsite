3ab7466679d6448b45a0a6c31132777ba98cfbcc
Sam Towers <jamisbrillx@gmail.com>
2021-09-12 11:19:28 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Phones Online</title>
+
+    <link href="singlePageTemplate.css" rel="stylesheet" type="text/css">
+
+
+    <!--The following script tag downloads a font from the Adobe Edge Web Fonts server for use within the web page. We recommend that you do not modify it.-->
+    <script>var __adobewebfontsappname__="dreamweaver"</script>
+    <script src="http://use.edgefonts.net/source-sans-pro:n2:default.js" type="text/javascript"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<!-- Main Container -->
+<img src="PhonesOnlineLogo.png" alt="Logo">
+<form align = "right">
+
+ 
+
+</form>
+<div class="container">
+    <!-- Navigation -->
+
+    <blockquote>
+        <h4 class="logo">&nbsp;</h4>
+    </blockquote>
+    <header> <a href="">
+
+    </a>
+        <nav>
+            <ul>
+                <li><a href="#hero">HOME</a></li>
+                <li><a href="PhoneCatalogue.html">Phone Catalogue</a></li>
+                <li> <a href="#contact">CONTACT</a></li>
+            </ul>
+        </nav>
+        <a href="https://www.google.com/url?sa=i&amp;source=images&amp;cd=&amp;ved=2ahUKEwjNmo72l6bnAhVi8OAKHYopCKMQjRx6BAgBEAQ&amp;url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F451724%2Fbuy_cart_moving_shopping_cart_shop_shopping_shopping_cart_shopping_trolley_icon&amp;psig=AOvVaw3qfMNszwCovu4kx2jRfnD_&amp;ust=1580297187162923"></a></header>
+    <p>Buy Pre-Owned Phones</p>
+    <p>&nbsp;View The Featured&nbsp; collection below&nbsp; &nbsp;&nbsp;
+
+    </p>
+<section class="hero" id="hero">
+  <h2 class="hero_header">&nbsp;</h2>
+	<table width="500" border="0" cellpadding="5">
+
+<tr>
+
+<td align="center" valign="center">
+<img src="Phones/OnePlus.png" alt="Oneplus " />
+<br />
+Caption text centered under the image.
+</td>
+
+<td align="center" valign="center">
+<img src="Phones/Samsung S3 Mini.jpg" alt="Samsung S3 here" />
+<br />
+Samsung S3 Mini&nbsp; </td>
+
+</tr>
+
+</table>
+        <p class="tagline">&nbsp;</p>
+
+
+
+  </section>
+    <!-- About Section -->
+    <section class="about" id="about">
+        <h2 class="hidden">About</h2>
+        <p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+        <p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+        <p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+    </section>
+    <!-- Stats Gallery Section -->
+    <div class="gallery">
+        <div class="thumbnail">
+            <h1 class="stats">1500</h1>
+            <h4>TITLE</h4>
+            <p>One line description</p>
+        </div>
+        <div class="thumbnail">
+            <h1 class="stats">2300</h1>
+            <h4>TITLE</h4>
+            <p>One line description</p>
+        </div>
+        <div class="thumbnail">
+            <h1 class="stats">7500</h1>
+            <h4>TITLE</h4>
+            <p>One line description</p>
+        </div>
+        <div class="thumbnail">
+            <h1 class="stats">9870</h1>
+            <h4>TITLE</h4>
+            <p>One line description</p>
+        </div>
+    </div>
+    <!-- Parallax Section -->
+    <section class="banner">
+        <h2 class="parallax">Our Phones&nbsp;</h2>
+        <em><strong><img src="../Downloads/galaxy-s10-black-back.png" width="470" height="594" alt=""/></strong></em>
+        <p class="parallax_description">We&nbsp; provide the highest quality of phones with vigourous testing to identify their gradig along with formating all devices to their original specificiation&nbsp; &nbsp; &nbsp;</p>
+    </section>
+    <!-- More Info Section -->
+    <footer>
+        <article class="footer_column">
+            <h3>Samsung's Line-up&nbsp;</h3>
+
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/K2dB12UMpUo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+            <p>Smartphone innovation with an infinity-o display: minimal, beautiful and effortlessly elegant; with no home button, no notch for the receiver and a simple dot opening for the front camera, the Galaxy S10+ gives you an uninterrupted viewing experience PowerShare: it gives you the superpower to share your power wherever you are, simply connect your Galaxy Watch or Galaxy Buds with the S10+ and charge them at the same time, or bring your friend's back to life True Vision Camera: the Galaxy S10+ captures every moment exactly as you see it with 3 rear cameras, an ultra-wide lens and 2 front cameras </p>
+        </article>
+        <article class="footer_column">
+            <h3>Apple's Line-Up&nbsp;&nbsp;</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/9m_K2Yg7wGQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p>5.8-inch Super Retina display (OLED) with HDR IP67 water and dust resistant (maximum depth of 1 metre up to 30 minutes) 12MP dual cameras with dual OIS and 7MP TrueDepth front camera — Portrait mode and Portrait Lighting Face ID for secure authentication and Apple Pay A11 Bionic with Neural Engine Wireless charging — works with Qi chargers </p>
+        </article>
+    </footer>
+    <!-- Footer Section -->
+    <section class="footer_banner" id="contact">
+        <h2 class="hidden">Footer Banner Section </h2>
+        <div class="BUTTON">
+        </div>
+        <div class="button">	  <a href = "STWebsite.html">Return To Top </a>
+            &nbsp;</div>
+    </section>
+    <!-- Copyrights Section -->
+    <div class="copyright">&copy;2020- <strong>Phones Online&nbsp;</strong></div>
+</div>
+<!-- Main Container Ends -->
+</body>
+</html>
